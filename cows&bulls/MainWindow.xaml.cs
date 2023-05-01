1f3b76d128836feb085ae0115842559ebcf0bff7
sh4mp0o <sh4mp0o@vk.com>
2023-05-01 15:05:52 +0300

    небольшие фиксы интерфейса + начало работы с логикой игры

--- cows&bulls/MainWindow.xaml.cs
+++ cows&bulls/MainWindow.xaml.cs
@@ -22,32 +22,40 @@ namespace cows_bulls
     {
         public void NewGame()
         {
-
+            listBox.Items.Add("Введите четырехзначное число без повторяющихся цифр.");
         }
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
         }
 
-        private void btn4_Click(object sender, RoutedEventArgs e)
+        private void tryButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btn1_Click(object sender, RoutedEventArgs e)
+        private void newGameButton_Click(object sender, RoutedEventArgs e)
         {
             NewGame();
         }
 
-        private void btn2_Click(object sender, RoutedEventArgs e)
+        private void recordsButton_Click(object sender, RoutedEventArgs e)
         {
             RecordWindow recordWindow = new RecordWindow();
             recordWindow.Show();
         }
 
-        private void btn3_Click(object sender, RoutedEventArgs e)
+        private void rulesButton_Click(object sender, RoutedEventArgs e)
         {
             RulesWindow rulesWindow = new RulesWindow();
+            rulesWindow.rulesLabel.Content = "Правила игры\r\nКомпьютер задумывает четыре различные цифры" +
+                " из 0,1,2,...9. Игрок делает\r\n ходы, чтобы узнать эти цифры и их порядок.\r\n\r\nКаждый ход" +
+                " состоит из четырёх цифр, 0 может стоять на первом месте.\r\n\r\nВ ответ компьютер" +
+                " показывает число отгаданных цифр, стоящих на\r\nсвоих местах (число быков) и число" +
+                " отгаданных цифр, стоящих не на\r\nсвоих местах (число коров).\r\n\r\nПример\r\nКомпьютер" +
+                " задумал 0834.\r\n\r\nИгрок сделал ход 8134.\r\n\r\nКомпьютер ответил:" +
+                " 2 быка (цифры 3 и 4) и 1 корова (цифра 8).";
             rulesWindow.Show();
         }
     }
