aad514dad4ffea1d994446910fe71422f6472ceb
sh4mp0o <sh4mp0o@vk.com>
2023-05-01 16:14:51 +0300

    реализован алгоритм загадывания числа компьютером

--- cows&bulls/MainWindow.xaml.cs
+++ cows&bulls/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace cows_bulls
 {
@@ -20,19 +8,54 @@ namespace cows_bulls
     /// </summary>
     public partial class MainWindow : Window
     {
+        string aim;
         public void NewGame()
         {
+            listBox.Items.Clear();
             listBox.Items.Add("Введите четырехзначное число без повторяющихся цифр.");
         }
         public MainWindow()
         {
             InitializeComponent();
             NewGame();
+            var rand = new Random();
+            int num;
+            bool[] used = new bool[10];
+            bool isUnique = true;
+            do
+            {
+                // сгенерировать случайное четырехзначное число
+                num = rand.Next(0, 10000);
+
+                // проверить, что все цифры числа уникальны и первая цифра не равна 0
+                Array.Clear(used, 0, used.Length);
+                foreach (char c in num.ToString())
+                {
+                    int digit = c - '0';
+                    if (used[digit] || (digit == 0 && c != '0'))
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                    used[digit] = true;
+                }
+            } while (!isUnique);
+            aim = Convert.ToString(num);
         }
 
         private void tryButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            string attemp = tbox.Text;
+            if (attemp.Length == 4 && Int32.TryParse(attemp, out var number))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Введите четырехзначное число!");
+                tbox.Text = "";
+            }
+
         }
 
         private void newGameButton_Click(object sender, RoutedEventArgs e)
@@ -50,7 +73,7 @@ namespace cows_bulls
         {
             RulesWindow rulesWindow = new RulesWindow();
             rulesWindow.rulesLabel.Content = "Правила игры\r\nКомпьютер задумывает четыре различные цифры" +
-                " из 0,1,2,...9. Игрок делает\r\n ходы, чтобы узнать эти цифры и их порядок.\r\n\r\nКаждый ход" +
+                " из 0,1,2,...9. Игрок делает\r\nходы, чтобы узнать эти цифры и их порядок.\r\n\r\nКаждый ход" +
                 " состоит из четырёх цифр, 0 может стоять на первом месте.\r\n\r\nВ ответ компьютер" +
                 " показывает число отгаданных цифр, стоящих на\r\nсвоих местах (число быков) и число" +
                 " отгаданных цифр, стоящих не на\r\nсвоих местах (число коров).\r\n\r\nПример\r\nКомпьютер" +
