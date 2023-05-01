694a76f9ed36f3f2ab28b93728378f0b5bf9cfee
sh4mp0o <sh4mp0o@vk.com>
2023-05-01 14:45:48 +0300

    добавлен интерфейс для игры

--- cows&bulls/MainWindow.xaml.cs
+++ cows&bulls/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace cows_bulls
     /// </summary>
     public partial class MainWindow : Window
     {
+        public void NewGame()
+        {
+
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +32,23 @@ namespace cows_bulls
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            RecordWindow recordWindow = new RecordWindow();
+            recordWindow.Show();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            RulesWindow rulesWindow = new RulesWindow();
+            rulesWindow.Show();
         }
     }
 }
